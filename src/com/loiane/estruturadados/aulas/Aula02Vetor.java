038f94736c3d9ffd7dd37cb17e3a2899fb88c374
Loiane Groner <loianeg@gmail.com>
2016-02-07 18:24:14 -0200

    aula 02

---
+++ src/com/loiane/estruturadados/aulas/Aula02Vetor.java
@@ -0,0 +1,13 @@
+package com.loiane.estruturadados.aulas;
+
+import com.loiane.estruturadados.Vetor;
+
+public class Aula02Vetor {
+
+	public static void main(String[] args) {
+		
+		//vetor com capacidade para 100 elementos
+		Vetor vetor = new Vetor(100);
+	}
+
+}
