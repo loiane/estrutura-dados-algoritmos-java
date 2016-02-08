3d156d86d2686b842af818fbaf644a6b02a930ce
Loiane Groner <loianeg@gmail.com>
2016-02-08 09:00:22 -0200

    aula 03

---
+++ src/com/loiane/estruturadados/aulas/Aula03Vetor.java
@@ -0,0 +1,16 @@
+package com.loiane.estruturadados.aulas;
+
+import com.loiane.estruturadados.Vetor;
+
+public class Aula03Vetor {
+
+	public static void main(String[] args) throws Exception {
+		
+		//vetor com capacidade para 10 elementos
+		Vetor vetor = new Vetor(10);
+
+		vetor.adiciona("Curso");
+		vetor.adiciona("Estrutura de Dados");
+	}
+
+}
