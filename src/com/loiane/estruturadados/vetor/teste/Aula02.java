00de2ac43f3926c1ed8db1a6bf6ceb7b4cb26916
Loiane Groner <loianeg@gmail.com>
2016-04-16 13:31:20 -0300

    aula 02

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula02.java
@@ -0,0 +1,12 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula02 {
+
+	public static void main(String[] args) {
+		
+		Vetor vetor = new Vetor(5);
+	}
+
+}
