d061595d6786f00450f83bccb176354171bfbec7
Loiane Groner <loianeg@gmail.com>
2016-04-16 13:31:24 -0300

    aula 03

---
+++ src/com/loiane/estruturadados/vetor/teste/Aula03.java
@@ -0,0 +1,16 @@
+package com.loiane.estruturadados.vetor.teste;
+
+import com.loiane.estruturadados.vetor.Vetor;
+
+public class Aula03 {
+
+	public static void main(String[] args) {
+
+		Vetor vetor = new Vetor(2);
+
+		vetor.adiciona("elemento 1");
+		vetor.adiciona("elemento 2");
+		vetor.adiciona("elemento 3");
+	}
+
+}
