8a5fae364158bb95c7c0dac9ccc7637b04ee3e1c
loiane <loianeg@gmail.com>
2016-08-22 11:47:48 -0300

    aula 19

---
+++ src/com/loiane/estruturadados/fila/Fila.java
@@ -0,0 +1,14 @@
+package com.loiane.estruturadados.fila;
+
+import com.loiane.estruturadados.base.EstruturaEstatica;
+
+public class Fila<T> extends EstruturaEstatica<T>{
+
+	public Fila(){
+		super();
+	}
+	
+	public Fila(int capacidade){
+		super(capacidade);
+	}
+}
---
+++ src/com/loiane/estruturadados/fila/teste/Aula19.java
@@ -0,0 +1,16 @@
+package com.loiane.estruturadados.fila.teste;
+
+import com.loiane.estruturadados.fila.Fila;
+
+public class Aula19 {
+
+	public static void main(String[] args) {
+		
+		Fila<Integer> fila = new Fila<>();
+		
+		System.out.println(fila.estaVazia()); //true
+		System.out.println(fila.tamanho()); // 0
+
+	}
+
+}
